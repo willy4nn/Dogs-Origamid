8466693b83c3f2162432f6e3469d0652decfdcd8
Willyan N Pereira <willy4nnp@gmail.com>
2023-04-18 15:49:42 -0300

    Validate Token

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LoginForm from './LoginForm'
-import LoginCreate from './LoginCreate'
-import LoginPasswordLost from './LoginPasswordLost'
-import LoginPasswordReset from './LoginPasswordReset'
-
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+  if (login === true) return <Navigate to='/conta'/>
   return (
     <div>
       <Routes>
-        <Route path='/' element={<LoginForm/>}/>
-        <Route path='/criar' element={<LoginCreate/>}/>
-        <Route path='/perdeu' element={<LoginPasswordLost/>}/>
-        <Route path='/resetar' element={<LoginPasswordReset/>}/>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/criar" element={<LoginCreate />} />
+        <Route path="/perdeu" element={<LoginPasswordLost />} />
+        <Route path="/resetar" element={<LoginPasswordReset />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,39 +3,32 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
+      userLogin(username.value, password.value);
     }
   }
-
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
